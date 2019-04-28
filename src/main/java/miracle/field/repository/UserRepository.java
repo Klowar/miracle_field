71efe767108258f17028979a3163f6dcff3b44f5
M.Aider <ideeer4@gmail.com>
2019-04-28 03:32:09 +0300

    UPDATE files to new .gitignore

--- src/main/java/miracle/field/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package miracle.field.repository;
-
-import miracle.field.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
