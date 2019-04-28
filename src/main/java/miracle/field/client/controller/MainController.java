15482ba0f9223d9477a245163954e1a6e6b37d4c
M.Aider <ideeer4@gmail.com>
2019-04-28 19:07:00 +0300

    REMOVE redudant class

--- src/main/java/miracle/field/client/controller/MainController.java
+++ src/main/java/miracle/field/client/controller/MainController.java
@@ -3,8 +3,6 @@ package miracle.field.client.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import miracle.field.client.util.ApplicationContexHolder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,16 +10,13 @@ public class MainController extends AbstractFxmlController {
     @FXML private TextField user;
     @FXML private TextField password;
     @FXML private Button loginButton;
-    private ApplicationContexHolder holder;
 
-    @Autowired
-    public MainController(ApplicationContexHolder holder){
-        this.holder = holder;
+    public MainController() {
     }
+
     @FXML
     public void onClickLoad() {
         System.out.println(getContext());
-        System.out.println(holder);
     }
 
 }
