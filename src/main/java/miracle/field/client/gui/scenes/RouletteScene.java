192e1f4ec2315c29adf97249234015d55aa53518
MerenovaAnastasiya <merenaas@yandex.ru>
2019-05-06 19:20:59 +0300

    changed roulette scene

--- src/main/java/miracle/field/client/gui/scenes/RouletteScene.java
+++ src/main/java/miracle/field/client/gui/scenes/RouletteScene.java
@@ -4,97 +4,139 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Arc;
 import javafx.scene.shape.ArcType;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextBoundsType;
 import javafx.util.Duration;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class RouletteScene extends Scene implements EventHandler<ActionEvent> {
     private Pane pane;
-    private Roulette roulette;
-    private Button turningButton;
+    private Sector[] sectors;
     private Timeline timeline;
+    private Button turningButton;
+    private Ellipse centerOfRoulette;
+    private final String[] textStrings = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"};
 
     public RouletteScene(Parent root, int quantityOfSectors)  {
         super(root);
         pane = new Pane();
-        roulette = createRoulette(quantityOfSectors);
+        sectors = createRouletteSectors(quantityOfSectors);
         turningButton = new Button("Крутить!");
-        turningButton.setOnAction(this);
-        addAllComponentsToScene((StackPane) root, roulette.sectors);
+        centerOfRoulette = new Ellipse(200, 200, 10,10);
+        addAllComponentsToScene((StackPane) root);
+        addAllActionsToScene();
     }
-    private Roulette createRoulette(int quantityOfSectors) {
-        Arc[] sectors = new Arc[quantityOfSectors];
-        int radius = 200;
+    private Sector[] createRouletteSectors(int quantityOfSectors) {
+        Sector[] sectors = new Sector[quantityOfSectors];
         double startAngle = 0;
         double length =  360.0 / quantityOfSectors;
         for (int i = 0; i < quantityOfSectors; i++) {
-            Arc arc = new Arc();
-            arc.setCenterX(0);
-            arc.setCenterY(0);
-            arc.setRadiusX(radius);
-            arc.setRadiusY(radius);
-            arc.setStartAngle(startAngle);
-            arc.setLength(length);
-            arc.setType(ArcType.ROUND);
-            arc.setLayoutX(radius);
-            arc.setLayoutY(radius);
-            sectors[i] = arc;
+            Sector sector = new Sector(textStrings[i], length, startAngle);
             startAngle += length;
+            sectors[i] = sector;
         }
         for (int i = 0; i < quantityOfSectors; i = i + 2) {
-            sectors[i].setFill(Color.BLACK);
+            sectors[i].color(Color.BLACK, Color.WHITE);
         }
         for (int i = 1; i < quantityOfSectors; i = i + 2) {
-            sectors[i].setFill(Color.WHITE);
-            sectors[i].setStroke(Color.BLACK);
+            sectors[i].color(Color.WHITE, Color.BLACK);
+
         }
-        return new Roulette(sectors);
-    }
-    private void addAllComponentsToScene(StackPane root, Arc[] sectors) {
-        for(int i = 0; i < sectors.length; i++) {
-            pane.getChildren().add(sectors[i]);
-        }
-        pane.getChildren().add(new Ellipse(200, 200, 10,10));
-        root.getChildren().addAll(pane, turningButton);
+        return sectors;
     }
 
-    @Data
-    @AllArgsConstructor
-    private class Roulette extends Node {
-        private Arc[] sectors;
+    private void addAllComponentsToScene(StackPane root) {
+        for (Sector sector : sectors) {
+            pane.getChildren().addAll(sector.getArc());
+        }
+        for (Sector sector : sectors) {
+            pane.getChildren().addAll(sector.getText());
+        }
+        pane.getChildren().add(centerOfRoulette);
+        root.getChildren().addAll(pane, turningButton);
     }
-//  Start animation
+    private void addAllActionsToScene(){
+        turningButton.setOnAction(this);
+    }
+    //  Start animation
     @Override
     public void handle(ActionEvent actionEvent) {
-        double milliseconds = 2 + Math.random() * 9;
+        double milliseconds = 5 + Math.random() * 9;
         timeline = new Timeline(new KeyFrame(Duration.millis(milliseconds), e -> spinRoulette()));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
     }
-    public void spinRoulette() {
-        Arc[] sectors = roulette.sectors;
-        for (int i = 0; i < sectors.length; i++) {
-            double newStartAngle = sectors[i].getStartAngle() + 1;
-            if(newStartAngle >= 360 ){
-                newStartAngle -=  360;
+    private void spinRoulette() {
+        for (Sector sector : sectors) {
+            double newStartAngle = sector.getArc().getStartAngle() + 1;
+            if (newStartAngle >= 360) {
+                newStartAngle -= 360;
             }
-            sectors[i].setStartAngle(newStartAngle);
-            System.out.println(newStartAngle);
+            sector.spin(newStartAngle);
         }
     }
     //  Stop animation
     public void stopSpin(){
         timeline.stop();
+    }
+    @Data
+    private class Sector {
+        private Arc arc;
+        private Text text;
+        private double length;
+        private double startAngle;
+        private final double CENTER = 0;
+        private final double LAYOUT = 200;
+        private static final double RADIUS = 200;
+
+        Sector(String textString, double length, double startAngle){
+            this.length = length;
+            this.startAngle = startAngle;
+            text = new Text(textString);
+            arc = new Arc();
+            arc.setCenterX(CENTER);
+            arc.setCenterY(CENTER);
+            arc.setRadiusX(RADIUS);
+            arc.setRadiusY(RADIUS);
+            arc.setLayoutX(LAYOUT);
+            arc.setLayoutY(LAYOUT);
+            arc.setStroke(Color.BLACK);
+            arc.setType(ArcType.ROUND);
+            arc.setLength(length);
+            arc.setStartAngle(startAngle);
+            text.setBoundsType(TextBoundsType.VISUAL);
+            text.setX(getXCoordinateToText());
+            text.setY(getYCoordinateToText());
+            text.setFont(Font.font ("Helvetica", FontWeight.BOLD,20));
+
+        }
+
+        double getXCoordinateToText() {
+            return RADIUS + 0.75 * RADIUS * Math.cos(Math.toRadians(startAngle  + length/ 2));
+        }
+        double getYCoordinateToText() {
+            return RADIUS - 0.75 * RADIUS * Math.sin(Math.toRadians(startAngle + length/ 2 ));
+        }
+        void spin(double startAngle){
+            setStartAngle(startAngle);
+            arc.setStartAngle(startAngle);
+            text.setX(getXCoordinateToText());
+            text.setY(getYCoordinateToText());
+        }
+        void color(Color arcColor, Color textColor){
+            arc.setFill(arcColor);
+            text.setFill(textColor);
+        }
     }
 }
