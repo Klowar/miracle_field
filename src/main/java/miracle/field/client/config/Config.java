eeb90f1acc8b2e5d2df8b601ff904679d9627178
M.Aider <ideeer4@gmail.com>
2019-05-06 16:51:38 +0300

    CREATE 2 beans - connector and object_mapper

--- src/main/java/miracle/field/client/config/Config.java
+++ src/main/java/miracle/field/client/config/Config.java
@@ -1,11 +1,17 @@
 package miracle.field.client.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import miracle.field.client.util.WebSocketServerConnector;
+import org.java_websocket.client.WebSocketClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @ComponentScan({
@@ -17,5 +23,20 @@ public class Config {
 
     @Resource
     private Environment env;
+
+    @Bean
+    public WebSocketClient connector() throws URISyntaxException {
+        WebSocketClient client = new WebSocketServerConnector(
+                new URI("ws://localhost:55443")
+        );
+        client.connect();
+        return client;
+    }
+
+    @Bean
+    public ObjectMapper mapper() {
+        return new ObjectMapper();
+    }
+
 
 }
