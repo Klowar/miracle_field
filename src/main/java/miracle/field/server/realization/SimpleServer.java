8fc0e0b8ebc1925965d0f131f2e6b5fd70d9fdd1
M.Aider <ideeer4@gmail.com>
2019-05-10 00:59:25 +0300

    ADD room logic to server

--- src/main/java/miracle/field/server/realization/SimpleServer.java
+++ src/main/java/miracle/field/server/realization/SimpleServer.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import miracle.field.server.handler.BaseHandler;
 import miracle.field.server.handler.Handler;
+import miracle.field.shared.model.User;
+import miracle.field.shared.model.Word;
 import miracle.field.shared.packet.Packet;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
@@ -21,16 +23,20 @@ import java.util.*;
 @Component
 public class SimpleServer extends WebSocketServer implements ApplicationContextAware {
 
+//    Constant values
     private ApplicationContext context;
     private ObjectMapper mapper;
-    private List<Room> rooms;
     private Handler handlerChain;
+//    Dynamic values
+    private Map<String, WebSocket> activeUsers;
+    private List<Room> rooms;
 
     public SimpleServer() {
         super( new InetSocketAddress("127.0.0.1", 55443));
         mapper = new ObjectMapper();
         rooms = new LinkedList<>();
         handlerChain = new BaseHandler();
+        activeUsers = new HashMap<>();
     }
 
     public SimpleServer(InetSocketAddress address) {
@@ -38,12 +44,13 @@ public class SimpleServer extends WebSocketServer implements ApplicationContextA
         mapper = new ObjectMapper();
         rooms = new LinkedList<>();
         handlerChain = new BaseHandler();
+        activeUsers = new HashMap<>();
     }
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
         //conn.send("Welcome to the server!"); //This method sends a message to the new client
-        System.out.println("Client connected");
+        System.out.println("Client connected: " + conn.getRemoteSocketAddress());
     }
 
     @Override
@@ -78,6 +85,8 @@ public class SimpleServer extends WebSocketServer implements ApplicationContextA
             conn.send(
                     mapper.writeValueAsBytes(p)
             );
+            if (!activeUsers.containsKey(p.getToken()) && !p.getToken().isEmpty())
+                activeUsers.put(p.getToken(), conn);
         } catch (IOException e) {
             System.out.println("Client send not Packet class");
         }
@@ -114,32 +123,137 @@ public class SimpleServer extends WebSocketServer implements ApplicationContextA
         this.start();
     }
 
+    public Room getRoomById(Integer id) {
+        return rooms.get(id);
+    }
+
+    public Room getLastRoom() {
+        return rooms.get(rooms.size() - 1);
+    }
+
+    public WebSocket getUserByToken(String token) {
+        return activeUsers.get(token);
+    }
+
+    public Room createNewRoom() {
+        Room newRoom = new Room();
+        return newRoom;
+    }
+
     //  I'm not quite agreed with this
-    private class Room {
-        //      String is a CSRF token
+    public class Room {
+
+        private Integer id;
+        private Queue<WebSocket> playerOrder;
         private Map<String, WebSocket> players;
         private String salt;
+        private String nextToken;
+        private Word word;
+        private boolean open;
 
-        public void writeToOne(Packet packet, String key) {
-            try {
-                players.get(key).send(
-                        mapper.writeValueAsBytes(packet)
-                );
-            } catch (JsonProcessingException e) {
-                System.out.println("Can not convert packet to bytes " + packet);
+        public Room() {
+            id = rooms.size();
+            playerOrder = new PriorityQueue<>();
+            players = new HashMap<>();
+            salt = String.valueOf(Math.random() * 1000);
+            open = true;
+        }
+
+        public Set<String> getPlayers() {
+            return players.keySet();
+        }
+
+        public void startGame() throws JsonProcessingException {
+            writeToRoom(
+                    new Packet<>("wordLength","", word.getWord().length())
+            );
+            writeToRoom(
+                    new Packet<>("wordDescription","", word.getDescription())
+            );
+            playerOrder.addAll(players.values());
+            nextTurn();
+        }
+
+        public void nextTurn() throws JsonProcessingException {
+            WebSocket s = playerOrder.poll();
+            playerOrder.add(s);
+            s.send(
+                    mapper.writeValueAsBytes(
+                            new Packet("startTurn","", salt)
+                    )
+            );
+            setNextToken(
+                    players.entrySet()
+                            .stream()
+                            .filter(entry -> s.equals(entry.getValue()))
+                            .map(Map.Entry::getKey)
+                            .findFirst().get()
+            );
+        }
+
+        public void writeToOne(Packet packet, String token) throws JsonProcessingException {
+            players.get(token).send(
+                    mapper.writeValueAsBytes(packet)
+            );
+        }
+
+        public void addPlayer(WebSocket webSocket, String token) {
+            players.put(token, webSocket);
+            if (players.size() > 3) {
+                open = false;
+                try {
+                    startGame();
+                } catch (JsonProcessingException e) {
+
+                }
             }
         }
 
-        public void writeToRoom(Packet packet) {
+        public void removePlayer(String token) {
+            players.remove(token);
+        }
+
+        public void removeAllPlayers() {
+            players.clear();
+        }
+
+        public WebSocket getNext() {
+            return playerOrder.peek();
+        }
+
+        public void setNext(String token) {
+            playerOrder.add(
+                    players.get(token)
+            );
+        }
+
+        public String getNextToken() {
+            return nextToken;
+        }
+
+        public boolean isNext(String token) {
+//          TODO more complex check
+            return (nextToken + salt).equals(token);
+        }
+
+        public void setNextToken(String nextToken) {
+            this.nextToken = nextToken;
+        }
+
+        public void writeToRoom(Packet packet) throws JsonProcessingException {
             for (WebSocket s : players.values()) {
-                try {
-                    s.send(
-                            mapper.writeValueAsBytes(packet)
-                    );
-                } catch (JsonProcessingException e) {
-                    System.out.println("Can not convert packet to bytes " + packet);
-                }
+                s.send(
+                        mapper.writeValueAsBytes(packet)
+                );
             }
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public boolean isOpen() {
+            return open;
         }
     }
 }
