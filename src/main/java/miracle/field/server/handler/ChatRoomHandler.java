d97a8b2497234d5f8acdbf1e70d526b8bc7e0f28
M.Aider <ideeer4@gmail.com>
2019-05-10 01:00:53 +0300

    ADD room handlers NOT TESTED

---
+++ src/main/java/miracle/field/server/handler/ChatRoomHandler.java
@@ -0,0 +1,41 @@
+package miracle.field.server.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import miracle.field.server.realization.SimpleServer;
+import miracle.field.shared.packet.Packet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatRoomHandler extends BaseRoomHandler {
+
+    private final SimpleServer server;
+
+    @Autowired
+    public ChatRoomHandler(SimpleServer server) {
+        this.type = "roomChat";
+        this.server = server;
+    }
+
+    @Override
+    public Packet handle(Packet message) {
+        if (!message.getType().equals(type))
+            return nextHandler.handle(message);
+
+        Packet returnPacket = null;
+        String user = message.getToken();
+        SimpleServer.Room room = server.getRoomById(
+                getUserRoomId(user)
+        );
+
+        try {
+            room.writeToRoom(
+                    new Packet(type,"", message.getData())
+            );
+        } catch (JsonProcessingException e) {
+            System.out.println("Can not write packet to room: " +  message.getType());
+        }
+
+        return returnPacket;
+    }
+}
---
+++ src/main/java/miracle/field/server/handler/FindRoomHandler.java
@@ -0,0 +1,57 @@
+package miracle.field.server.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import miracle.field.server.realization.SimpleServer;
+import miracle.field.shared.packet.Packet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FindRoomHandler extends BaseRoomHandler {
+
+    private final SimpleServer server;
+
+    @Autowired
+    public FindRoomHandler(SimpleServer server) {
+        this.type = "findRoom";
+        this.server = server;
+    }
+
+    @Override
+    public Packet handle(Packet message) {
+        if (!message.getType().equals(type))
+            return nextHandler.handle(message);
+
+        Packet returnPacket = null;
+        String user = message.getToken();
+        SimpleServer.Room room = server.getLastRoom();
+
+        try {
+            if (room.isOpen()) {
+                room.addPlayer(
+                        server.getUserByToken(user),
+                        user
+                );
+            }
+            else{
+                room = server.createNewRoom();
+                room.addPlayer(
+                        server.getUserByToken(user),
+                        user
+                );
+            }
+            addUserToRoom(
+                    user,
+                    room.getId()
+            );
+            returnPacket = new Packet<>(type + "Success", "", room.getId());
+        } catch (JsonProcessingException constraintException) {
+//          TODO more info to user ???
+            returnPacket = new Packet<>(type + "Error", "", "");
+        }
+
+        return returnPacket;
+    }
+
+
+}
