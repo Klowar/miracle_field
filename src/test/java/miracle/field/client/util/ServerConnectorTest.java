69cb55876e42ce6a181de3170ab9de546b147b26
M.Aider <ideeer4@gmail.com>
2019-05-11 17:58:43 +0300

    Simple test

---
+++ src/test/java/miracle/field/client/util/ServerConnectorTest.java
@@ -0,0 +1,74 @@
+package miracle.field.client.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import miracle.field.shared.packet.Packet;
+import org.java_websocket.client.WebSocketClient;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(loader= AnnotationConfigContextLoader.class)
+public class ServerConnectorTest {
+
+    @Configuration
+    static class ContextConfiguration {
+
+        // this bean will be injected into the OrderServiceTest class
+        @Bean
+        public WebSocketClient connector() throws URISyntaxException {
+            WebSocketClient client = new WebSocketServerConnector(
+                    new URI("ws://localhost:55443")
+            );
+            client.connect();
+            return client;
+        }
+        @Bean
+        public ObjectMapper mapper() {
+            return new ObjectMapper();
+        }
+
+        @Bean
+        public Observer observer() {
+            return new Observer();
+        }
+    }
+
+    @Autowired
+    private WebSocketClient connector;
+    @Autowired
+    private ObjectMapper mapper;
+    @Autowired
+    private Observer observer;
+    private Waiter waiter;
+
+    @Before
+    public void setUp() {
+        waiter = packet -> System.out.println(packet.getType());
+    }
+
+    @Test
+    public void testRoomChat() throws JsonProcessingException {
+        waiter = packet -> Assert.assertEquals("loginError", packet.getType());
+        observer.addWaiter("loginError", waiter);
+        observer.addWaiter("loginSuccess", waiter);
+        connector.send(
+                mapper.writeValueAsBytes(
+                        new Packet<>("login","", "hello")
+                )
+        );
+    }
+
+}
