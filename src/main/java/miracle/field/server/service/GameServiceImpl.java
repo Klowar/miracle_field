0c23dfc559f92a2bcbfdf64f6d1ee61aca655ce7
M.Aider <ideeer4@gmail.com>
2019-05-14 19:11:12 +0300

    UPDATE and ADD some necessary methods

--- src/main/java/miracle/field/server/service/GameServiceImpl.java
+++ src/main/java/miracle/field/server/service/GameServiceImpl.java
@@ -3,44 +3,66 @@ package miracle.field.server.service;
 import miracle.field.server.gameData.MiracleFieldInfo;
 import miracle.field.server.repository.UserRepository;
 import miracle.field.server.repository.WordRepository;
+import miracle.field.shared.model.Statistic;
 import miracle.field.shared.model.Word;
-import org.java_websocket.WebSocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class GameServiceImpl implements GameService<MiracleFieldInfo> {
 
     private WordRepository wordRepository;
     private StatisticService statisticService;
-    private UserRepository userRepository;
+    private UserService userService;
 
     @Autowired
     public GameServiceImpl(WordRepository wordRepository,
-                           StatisticService statisticService, UserRepository userRepository) {
+                           StatisticService statisticService, UserService userService) {
         this.wordRepository = wordRepository;
         this.statisticService = statisticService;
-        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
 
     @Override
-    public void startGame(Map<String, WebSocket> players,
+    public void startGame(Collection<String> players,
                           MiracleFieldInfo gameInfo) {
-//      ToDo: something else??
         Word word = wordRepository.getRandomWord();
         gameInfo.setWord(word);
     }
 
     @Override
     public void finishedGame(MiracleFieldInfo gameInfo) {
-
+        Long id = userService.getAuthorizedUser(gameInfo.getWinner()).getId();
+        Statistic statistic = statisticService.getUserStatistic(id);
+        statistic.setScore(
+                statistic.getScore() + gameInfo.getPlayersScore().get(gameInfo.getWinner())
+        );
+        statisticService.updateUserStatistic(
+                statistic
+        );
     }
 
     @Override
-    public void gameMove(MiracleFieldInfo gameInfo, String player) {
+    public void gameMove(MiracleFieldInfo gameInfo, String player, String data) {
+        String currentPlayer = gameInfo.getCurrentPlayer();
+        if (!currentPlayer.equals(player))
+            return;
+        String word = Arrays.toString(gameInfo.getOpenLetters());
+        if (!word.contains(data)) {
+            gameInfo.setChangeTurn(false);
+            gameInfo.setOpenLetters((word + data).toCharArray());
+        }
+
+        if (Arrays.equals(gameInfo.getWord().toCharArray(),(word + data).toCharArray()) ||
+        Arrays.equals(gameInfo.getWord().toCharArray(), data.toCharArray())) {
+            gameInfo.setWinner(player);
+        }
 
     }
+
 }
--- src/main/java/miracle/field/server/service/UserService.java
+++ src/main/java/miracle/field/server/service/UserService.java
@@ -5,5 +5,6 @@ import miracle.field.shared.model.User;
 public interface UserService {
     void signUp(User user);
     User login(User user);
+    User getAuthorizedUser(String token);
     String getToken(User user);
 }
